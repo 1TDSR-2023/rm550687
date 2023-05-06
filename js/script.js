16c3ed83e3a234385f87a90a95266c86090ed01e
PhabloFiap <rm550687@fiap.com.br>
2023-05-05 22:13:40 -0300

    Array declarations/ functions/

--- js/script.js
+++ js/script.js
@@ -15,22 +15,89 @@
 // console.log(nome1);
 /* <button id="meu-btn">CLICK</button> */
 
-let botao = document.getElementById("meu-btn");
-// console.log(botao.textContent);
+// let botao = document.getElementById("meu-btn");
+// // console.log(botao.textContent);
 
-botao.addEventListener("click", function(){ 
-    // console.log(this.textContent);
-    //Função matemática Math
-    //randon = Retorna um número aleatório entre 0 e 1. Ex: 0.2282632765;
-    //floor  = Ela arredonda um número para baixo.
-    //ceil   = Ela arredonda um número para cima.
-    //round  = Ela arredonda um número aleatóriamente.
+// botao.addEventListener("click", function(){ 
+//     // console.log(this.textContent);
+//     //Função matemática Math
+//     //randon = Retorna um número aleatório entre 0 e 1. Ex: 0.2282632765;
+//     //floor  = Ela arredonda um número para baixo.
+//     //ceil   = Ela arredonda um número para cima.
+//     //round  = Ela arredonda um número aleatóriamente.
 
-    let r=0,g=0,b=0;
-    r = Math.round(Math.random()*255);
-    g = Math.round(Math.random()*255);
-    b = Math.round(Math.random()*255);
+//     let r=0,g=0,b=0;
+//     r = Math.round(Math.random()*255);
+//     g = Math.round(Math.random()*255);
+//     b = Math.round(Math.random()*255);
     
-    this.setAttribute("style",`background-color:rgb(${r},${g},b);`);
+//     this.setAttribute("style",`background-color:rgb(${r},${g},b);`);
 
-});
+// });
+
+//DECLARAR UM ARRAY
+// let frutas = ["banana", "maçã", "pêssego", "laranja", "limão", "melancia", "kiwi"];
+// //imprimindo o array
+// console.log (frutas);
+// console.table(frutas);
+// console.table(frutas[0]);
+// //inserir m item ao final do array com o metodo push();
+
+// frutas.push("manga");
+// console.table()
+
+// //inserir m item ao inicio do array com o metodo unshift();
+// frutas.unshift ("pitaya");
+
+// //excluir um item ao final do array com o metodo pop();
+// frutas.pop();
+// //excluir um item ao inicio do array com o metodo shift();
+// frutas.shift();
+
+// //localizar um item no array utilizando o metodo indexOf(nomeDoItem);
+// //Obs: esse metodo retorna o indice do item encontrado.
+
+// // let indice = frutas.indexOf("laranja");
+// // console.log (`O indice encontrado foi o: ${indice}` );
+// // console.log (`O item buscado foi o o: ${frutas [indice]}` );
+
+// //realizando uma remocao de item utilizando o metodo splice()
+// //O metodo splice() recebe dois parametros, o indice do item que sera removido e a quantidade de vezes que este mesmo indice sera removido. Ex: splice(indice, 1);
+// //Dica utilize o metodo indexOf() para buscar o indice do item do array.
+
+// let indice = frutas.indexOf("melancia");
+//  console.log (`O item encontrado do indice antes da remoção foi : ${frutas[indice]}` );
+// frutas.splice (indice,1);
+// console.log (frutas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
